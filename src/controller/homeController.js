4188ec973232d4ec4b965aea38472506bab0c110
dvminh-it <dvminh.it@gmail.com>
2023-02-02 17:20:05 +0700

    Hoàn thiện crud

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -15,8 +15,29 @@ let createNewUser = async (req, res) => {
     await pool.execute('insert into users(firstName, lastName, email, address) values (?, ?, ?, ?)',  [firstName, lastName, email, address]);
    return res.redirect('/')
 }
+
+let deleteUser = async (req, res) => {
+    await pool.execute('delete from users where id = ?', [req.body.id])
+    res.redirect('/')}
+
+let getEditPage = async (req, res) => {
+    let [user] = await pool.execute('SELECT * FROM users WHERE id = ?', [req.params.id]);
+    return res.render('update.ejs', { dataUser: user[0] });
+}
+
+let postUpdateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+    await pool.execute('UPDATE users SET firstName= ?, lastName = ? , email = ? , address= ? WHERE id = ?',
+        [firstName, lastName, email, address, id]);
+
+    return res.redirect('/');
+}
+
 export default {
     getHomePage,
     getDetailPage,
-    createNewUser
+    createNewUser,
+    deleteUser,
+    getEditPage,
+    postUpdateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,9 @@ const intWebRoute = (app) => {
     router.get('/', homeController.getHomePage)
     router.get('/detail/user/:id', homeController.getDetailPage)
     router.post('/create/user', homeController.createNewUser)
+    router.post('/delete/user', homeController.deleteUser)
+    router.get('/edit/user/:id', homeController.getEditPage)
+    router.post('/update/user', homeController.postUpdateUser);
     return app.use('/', router)
 }
 
