09235e8a75e81dc46c4c9d30168476dbceb36368
dvminh-it <dvminh.it@gmail.com>
2023-02-05 08:42:59 +0700

    Upload single file

--- src/route/web.js
+++ src/route/web.js
@@ -1,15 +1,47 @@
 import express from 'express';
 import homeController from '../controller/homeController';
+import multer from 'multer';
+import path from 'path';
+var appRoot = require('app-root-path');
 let router = express.Router();
+
+const storage = multer.diskStorage({
+  
+    destination: function (req, file, cb) {  console.log('app root' + appRoot);
+        cb(null,appRoot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
 
 const intWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/detail/user/:id', homeController.getDetailPage);
+
     router.get('/create/user', homeController.getCreatePage);
     router.post('/create/user', homeController.createNewUser);
+
     router.post('/delete/user', homeController.deleteUser);
     router.get('/edit/user/:id', homeController.getEditPage);
     router.post('/update/user', homeController.postUpdateUser);
+
+    router.get('/upload', homeController.getUploadFilePage);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
+
     return app.use('/', router);
 };
 
